cfd906e28a2add0fcc5835becaa8a2fdf9446abe
Artur Barilo <efreyu@gmail.com>
2024-09-06 22:43:32 +0300

    master | add helpers and utils \wip

---
+++ src/core/assert.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef ANIMAFLOW_CORE_MODULE_ASSERT_HPP
+#define ANIMAFLOW_CORE_MODULE_ASSERT_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#ifndef NDEBUG
+#if defined(__GNUC__) || defined(__clang__)
+#define GENERATE_TRAP() __builtin_trap()  // GCC/Clang specific trap
+#elif defined(_MSC_VER)
+#include <intrin.h>
+#define GENERATE_TRAP() __debugbreak()    // MSVC specific trap
+#else
+#define GENERATE_TRAP() raise(SIGTRAP)    // Portable alternative using SIGTRAP
+#endif
+//
+// In debug mode, checks the passed in condition and outputs
+// detailed information to stederr, including a custom error
+// message when the condition evaluates to false.
+//
+#define assertion(condition, message)                                               \
+    do {                                                                            \
+        if (!(condition)) [[unlikely]] {                                            \
+            std::cerr << __FUNCTION__ << __FILE__ << __LINE__ <<                    \
+                                      message " => condition: (" #condition ")";    \
+            std::cerr.flush();                                                      \
+            GENERATE_TRAP();                                                        \
+        }                                                                           \
+    } while (false)
+
+#define error_msg_base(message)                                                     \
+    do {                                                                            \
+        std::cerr << __FUNCTION__ << __FILE__ << __LINE__ << message;               \
+        std::cerr.flush();                                                          \
+        GENERATE_TRAP();                                                            \
+    } while (false)
+#define error_msg(format_str, ...)                                                  \
+    do {                                                                            \
+        std::string formatted_msg = std::format(format_str, __VA_ARGS__);           \
+        error_msg_base(formatted_msg);                                              \
+    } while (false)
+
+//#define info_msg_base(message)                                                                    \
+//    do {                                                                                          \
+//        ::godot::UtilityFunctions::print("[INFO] (", __FUNCTION__, ") ", __FILE__, ":", __LINE__, \
+//                                         " ", message);                                           \
+//    } while (false)
+//#define info_msg(...) info_msg_base(godot::UtilityFunctions::str(__VA_ARGS__))
+
+#define runtime_assert(condition) assertion(condition, "validation check failed")
+
+#else
+//
+// In release mode the macro does nothing ((void)0), including
+// the execution of the condition so don't define the expression
+// as anything that would be considered program logis.
+//
+#define assertion(condition, message)   static_cast<void>(0)
+#define error_msg_base(message)         static_cast<void>(0)
+#define error_msg(format_str, ...)      static_cast<void>(0)
+//#define info_msg_base(message)          static_cast<void>(0)
+//#define info_msg(...)                   static_cast<void>(0)
+#define runtime_assert(condition)       static_cast<void>(0)
+#endif
+
+#endif  // ANIMAFLOW_CORE_MODULE_ASSERT_HPP
---
+++ src/core/attributes.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ANIMAFLOW_CORE_MODULE_ATTRIBUTES_HPP
+#define ANIMAFLOW_CORE_MODULE_ATTRIBUTES_HPP
+
+namespace af::inline utils {
+
+#if defined(_MSC_VER)
+        // MSVC doesn't have a clean way to ignore custom
+        // attributes within a namespace like clang or gcc
+#define _property
+#define _signal_slot
+#define _method
+#elif defined(__GNUG__) || defined(__clang__)
+        // these macros are used to define custom attributes to label godot signal callbacks
+        // and node properties. they don't do anything other than making it easier to spot
+        // these functions when reading the code or searching for them in an IDE.
+#define _property    __attribute__((af("property")))
+#define _signal_slot __attribute__((af("signal_slot")))
+#define _method      __attribute__((af("method")))
+#endif
+
+}  // namespace af::inline utils
+
+#endif  // ANIMAFLOW_CORE_MODULE_ATTRIBUTES_HPP
